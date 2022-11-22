547ca95401e632c061d9707c7da79441f7ba1d1d
Lucas Freitas Gonçalves Ribeiro <lucasfreitasr@outlook.com>
2022-11-21 23:49:09 -0300

    novas aulas

---
+++ Udemy/aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print(f'adicao: {adicao}')
+
+subtracao = 10 - 5
+print(f'adicao: {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'Multiplicação: {multiplicacao}')
+
+divisao = 10 / 2.2
+print(f'Divisão: {divisao}')
+
+divisao_inteira = 10 // 2.2
+print(f'Divisão inteira: {divisao_inteira}')
+
+exponenciaocao = 2 ** 10
+print(f'Exponenciação: {exponenciaocao}')
+
+modulo = 25 % 2 # Resto da divisão
+print(f'Módulo: {modulo}')
